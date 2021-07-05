27d63eda518415a47b04bb8cfbf14708009cddf2
gilan-star <83273499+gilan-star@users.noreply.github.com>
2021-07-05 19:29:45 +0900

    detach common module

--- js/main.js
+++ js/main.js
@@ -1,22 +1,3 @@
-// SEARCH ICON
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function () {
-    searchInputEl.focus()
-});
-
-searchInputEl.addEventListener('focus', function () {
-    searchEl.classList.add('focused');
-    searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function () {
-    searchEl.classList.remove('focused');
-    searchInputEl.setAttribute('placeholder', '');
-});
-
-
 // FADING OUT BADGES WHEN IT IS SCROLLED
 // const badgeEl = document.querySelector('header .badges');
 
@@ -105,6 +86,3 @@ spyEls.forEach(function (spyEl) {
         .addTo(new ScrollMagic.Controller());
 });
 
-// GET FULL YEAR FOR COPYRIGHT
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
